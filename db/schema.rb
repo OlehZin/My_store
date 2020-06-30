14e9a0bd87a1c4139a0cfe7b1927f0bc9fe86244
OlehZin <biosoln91@gmail.com>
2020-06-30 12:37:44 +0300

    second commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_29_122722) do
+ActiveRecord::Schema.define(version: 2020_06_30_060228) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "name"
+    t.text "message"
+    t.integer "phone"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "faqs", force: :cascade do |t|
     t.string "title"
