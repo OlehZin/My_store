bf9c19f03e795061fd086980bbacf95989813c58
OlehZin <biosoln91@gmail.com>
2020-06-30 18:01:56 +0300

    _somefixes commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.create(user_params)
-    redirect_to products_path(@product)
+    redirect_to products_path
   end
 
   def update
--- app/models/user.rb
+++ app/models/user.rb
@@ -16,14 +16,12 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :confirmable, :lockable
+         :recoverable, :rememberable, :validatable
+         # :confirmable, :lockable
 
-enum role: [:user, :admin]
-after_initialize :set_default_role, :if => :new_record?
+  enum role: [:user, :admin]
 
-  def set_default_role
-    self.role ||= :user
-  end
+
+
 
 end
